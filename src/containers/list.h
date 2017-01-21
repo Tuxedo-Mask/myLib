fdfe542813a829b4cccd531c12b819eb4a834f8e
Tuxedo-Mask <haykhakobyan89@gmail.com>
2017-01-21 18:28:50 -0500

    Added dinamically allocated List

--- src/containers/list.h
+++ src/containers/list.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+#include "utils.h"
+
 namespace myLib
 {
 
@@ -37,13 +39,13 @@ public:
     void removeFront();
     void pushBack(T value);
     void removeBack();
-    unsigned int size() const;
+    size_t size() const;
     bool empty() const ;
     void print() const;
 private:
     ListNode<T> * m_head;
     ListNode<T> * m_tail;
-    unsigned int m_size;
+    size_t m_size;
 };
 
 template <typename T>
@@ -71,7 +73,7 @@ void List<T>::pushFront(T value)
     ListNode<T>* node = new ListNode<T>();
     node->m_value = value;
     if (empty()) {
-        m_tail = node;
+        m_head = m_tail = node;
     } else {
         node->next = m_head;
         m_head->prev = node;
@@ -85,18 +87,50 @@ void List<T>::removeFront()
 {
     if (!empty()) {
         ListNode<T>* temp = m_head;
-        m_head->next->prev = 0;
-        m_head = m_head->next;
+        if (1 == size()) {
+            m_head = m_tail = 0;
+        } else {
+            m_head->next->prev = 0;
+            m_head = m_head->next;
+        }
         delete temp;
         --m_size;
-        if (1 == size()) {
-            m_head = m_prev;
-        }
     }
 }
 
 template <typename T>
-unsigned int List<T>::size() const
+void List<T>::pushBack(T value)
+{
+    ListNode<T>* node = new ListNode<T>();
+    node->m_value = value;
+    if (empty()) {
+        m_head = m_tail = node;
+    } else {
+        node->prev = m_tail;
+        m_tail->next = node;
+        m_tail = node;
+    }
+    ++m_size;
+}
+
+template <typename T>
+void List<T>::removeBack()
+{
+    if (!empty()) {
+        ListNode<T>* temp = m_tail;
+        if (1 == size()) {
+            m_head = m_tail = 0;
+        } else {
+            m_tail->prev->next = 0;
+            m_tail = m_tail->prev;
+        }
+        delete temp;
+        --m_size;
+    }
+}
+
+template <typename T>
+size_t List<T>::size() const
 {
     return m_size;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
-#include "staticHeap.h"
-
-const int array_size = 10;
+#include "list.h"
 
 int main()
 {
-    int array[] = {4, 1, 3, 2, 16, 9, 10, 14, 8, 7};
-    myLib::staticHeap<int>::heapSort(array, array_size, myLib::less_or_equal);
-    myLib::print(array, array_size);
+    myLib::List<int> myList;
+
+    myList.print();
     
     return 0;
 }
